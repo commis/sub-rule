67fe720a83c05bac5ebb54a1dfb9b036e7db370c
bing.xu <bing.xu@lingshu.net>
2023-12-17 17:46:26 +0800

    commit message

---
+++ remove-commits.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT_DIR=$(cd "$(dirname "$(readlink -f "$0")")" && pwd)
+
+function del_history_commit() {
+    git checkout --orphan latest_branch
+    git add -A
+    git commit -am "commit message"
+    git branch -D main
+    git branch -m main
+    git push -f origin main
+}
+
+# main function code
+OLD=$(pwd)
+del_history_commit
+cd "$OLD" || exit
